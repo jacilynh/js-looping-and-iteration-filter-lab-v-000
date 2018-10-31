5707c75b1e7c962dad5d922ec64e5a28a3ee3d63
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-31 06:16:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file
+// returns all drivers that match the passed in name
+// returns matching drivers if case does not match but letters do
+// returns an empty array if there is no match
+function findMatching() {
+  
+}
+
+
+// returns a driver if beginning provided letters match
+// does not return drivers if only middle or ending letters match
+// does not return drivers if only middle or ending letters match
+// function fuzzyMatch() {
+  
+// }
+
+
+// accesses the data structure to check if name matches
+// function matchName() {
+  
+// }
