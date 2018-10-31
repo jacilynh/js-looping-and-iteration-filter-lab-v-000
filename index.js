96c96a8979ae79c10962b3fd23982e511abfcd92
Jacilyn Hayden <jacihayden@gmail.com>
2018-10-31 06:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,18 @@ function findMatching(drivers, name) {
 // returns a driver if beginning provided letters match
 // does not return drivers if only middle or ending letters match
 // does not return drivers if only middle or ending letters match
-// function fuzzyMatch() {
-  
-// }
+function fuzzyMatch(drivers, subString) {
+  let stringList = subString.length
+  return drivers.filter(function(driverName) {
+    return driverName.slice(0, stringList) === subString
+  })
+}
+
 
 
 // accesses the data structure to check if name matches
-// function matchName() {
-  
-// }
+ function matchName(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.name === name
+  })
+}
